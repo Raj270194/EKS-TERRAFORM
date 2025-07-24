0bca5d6e7d7c68b0a4aba581e0c12ce84d437158
user <bontha-rajashekar.bontha-rajashekar@capgemini.com>
2025-07-24 13:42:55 +0530

    changed bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "rajpractice" # Replace with your actual S3 bucket name
+    bucket = "rajpractice111" # Replace with your actual S3 bucket name
     key    = "Eks/terraform.tfstate"
     region = "eu-north-1"
   }
